7088d3873d90fc0540eafca8de169cdb4276a372
Adushaiq <addushaiq367@gmail.com>
2024-12-27 16:47:56 +0530

    Updated Products Json

--- src/jsons/products.js
+++ src/jsons/products.js
@@ -28,164 +28,165 @@ export const products = [
     "brand": "Flora"
   },
   {
-    "id": 11,
+    "id": 5,
     "name": "AI ST 29 Steel Square Tower Bolt",
     "description": "9mm Rod",
     "image": "https://res.cloudinary.com/dx94piejq/image/upload/v1735198770/IMG_3264_jbf49e.jpg",
     "brand": "Flora"
   },
   {
-    "id": 12,
+    "id": 6,
     "name": "AI ST 02 Steel Square Delux Tower Bolt",
     "description": "9mm Rod",
     "image": "https://res.cloudinary.com/dx94piejq/image/upload/v1735198769/IMG_3262_meqptb.jpg",
     "brand": "Flora"
   },
   {
-    "id": 13,
+    "id": 7,
     "name": "AI SBL 05 Steel Rounded Baby Latch",
     "description": "9mm Baby Latch",
     "image": "https://res.cloudinary.com/dx94piejq/image/upload/v1735198770/IMG_3265_yyduws.jpg",
     "brand": "Flora"
   },
   {
-    "id": 15,
+    "id": 8,
     "name": "Floor Mounted Door Stop",
     "description": "Prima, Deluxe, Half Mood Door Stop",
     "image": "https://res.cloudinary.com/dx94piejq/image/upload/v1735198780/IMG_3269_dd6jpb.jpg",
     "brand": "Flora"
   },
   {
-    "id": 16,
+    "id": 9,
     "name": "Gate Hooks AI SGH 01, 02, 03",
     "description": "50mm 100mm, 150mm, 200mm",
     "image": "https://res.cloudinary.com/dx94piejq/image/upload/v1735198786/IMG_3268_uaqo6v.jpg",
     "brand": "Flora"
   },
   {
-    "id": 17,
+    "id": 10,
     "name": "Curtain Brackets",
     "description": "19mm, 25mm",
     "image": "https://res.cloudinary.com/dx94piejq/image/upload/v1735198780/IMG_3270_wekoug.jpg",
     "brand": "Flora"
   },
   {
-    "id": 18,
+    "id": 11,
     "name": "Sofa Leg",
     "description": "50mm, 75mm, 100mm, 150mm",
     "image": "https://res.cloudinary.com/dx94piejq/image/upload/v1735198784/IMG_3272_w1ckiq.jpg",
     "brand": "Flora"
   },
   {
-    "id": 19,
+    "id": 12,
     "name": "AI Sleek O Matic",
     "description": "Door Closer",
     "image": "https://res.cloudinary.com/dx94piejq/image/upload/v1735205948/IMG_3363_a8uibl.jpg",
     "brand": "Flora"
   },
   {
-    "id": 20,
+    "id": 13,
     "name": "Brass Tower Bolts",
     "description": "Depth  8mm, 9mm, 12mm Rods",
     "image": "https://res.cloudinary.com/dx94piejq/image/upload/v1735200710/FLORA_PL_lhoy8p.jpg",
     "brand": "Flora"
   },
   {
-    "id": 21,
+    "id": 14,
     "name": "Brass Door & Window Fittings",
     "description": "Fancy And Premium Gate Hooks",
     "image": "https://res.cloudinary.com/dx94piejq/image/upload/v1735200697/FLORA_PL_16_fkue31.jpg",
     "brand": "Flora"
   },
   {
-    "id": 22,
+    "id": 15,
     "name": "Butt Hinges",
     "description": "Depth 1.5mm, 2mm, 3mm Hinges",
     "image": "https://res.cloudinary.com/dx94piejq/image/upload/v1735200710/FLORA_PL_8_ptpxh6.jpg",
     "brand": "Flora"
   },
   {
-    "id": 23,
+    "id": 16,
     "name": "W Hinges",
     "description": "Depth 6mm, 12mm, 19mm, 25mm Hinges",
     "image": "https://res.cloudinary.com/dx94piejq/image/upload/v1735200708/FLORA_PL_7_gifpuh.jpg",
     "brand": "Flora"
   },
   {
-    "id": 24,
+    "id": 17,
     "name": "Overlay Hinges",
     "description": "Full & Half Overlay Hinges",
     "image": "https://res.cloudinary.com/dx94piejq/image/upload/v1735200705/FLORA_PL_6_vtcoxo.jpg",
     "brand": "Flora"
   },
   {
-    "id": 25,
+    "id": 18,
     "name": "Spring Hinges",
     "description": "Single & Double Action Hinges",
     "image": "https://res.cloudinary.com/dx94piejq/image/upload/v1735200702/FLORA_PL_5_hm2xep.jpg",
     "brand": "Flora"
   },
   {
-    "id": 26,
+    "id": 19,
     "name": "Z Type Hinges SST",
     "description": "12mm, 19mm, 25mm Hinges",
     "image": "https://res.cloudinary.com/dx94piejq/image/upload/v1735200703/FLORA_PL_4_ykl4h2.jpg",
     "brand": "Flora"
   },
   {
-    "id": 27,
+    "id": 20,
     "name": "Bearing Hinges",
     "description": "Bearing Hinges with Flat Tips",
     "image": "https://res.cloudinary.com/dx94piejq/image/upload/v1735200697/FLORA_PL_3_ybads9.jpg",
     "brand": "Flora"
   },
   {
-    "id": 28,
+    "id": 21,
     "name": "Special Bearing Pivot Hinges",
     "description": "19mm, 25mm, 31mm, Hinges",
     "image": "https://res.cloudinary.com/dx94piejq/image/upload/v1735200691/FLORA_PL_2_zzkgsw.jpg",
     "brand": "Flora"
   },
   {
-    "id": 29,
+    "id": 22,
     "name": "Cabinet Stopper",
     "description": "50mm, 63mm, 75mm",
     "image": "https://res.cloudinary.com/dx94piejq/image/upload/v1735200704/FLORA_PL_19_nlnmnk.jpg",
     "brand": "Flora"
   },
   {
-    "id": 30,
+    "id": 23,
     "name": "Premium Kent Towerbolt",
     "description": "Premimum & Bullet Towerbolts",
     "image": "https://res.cloudinary.com/dx94piejq/image/upload/v1735200690/FLORA_PL_18_hlx5ki.jpg",
     "brand": "Flora"
   },
   {
-    "id": 31,
+    "id": 24,
     "name": "Delux Baby Latches",
     "description": "50mm, 75mm, 100mm",
     "image": "https://res.cloudinary.com/dx94piejq/image/upload/v1735200706/FLORA_PL_17_y4ffjp.jpg",
     "brand": "Flora"
   },
   {
-    "id": 32,
+    "id": 25,
     "name": "Gate Hooks",
     "description": "50m, 63mm, 75mm, 100m Hooks",
     "image": "https://res.cloudinary.com/dx94piejq/image/upload/v1735200696/FLORA_PL_15_by8avs.jpg",
     "brand": "Flora"
   },
   {
-    "id": 33,
+    "id": 26,
     "name": "Round Aldrop",
     "description": "12mm, 16mm, 19mm Aldrops",
     "image": "https://res.cloudinary.com/dx94piejq/image/upload/v1735200706/FLORA_PL_14_ulju0v.jpg",
     "brand": "Flora"
   },
   {
-    "id": 34,
+    "id": 27,
     "name": "Handle",
     "description": "Premium Handles - 150mm, 175mm, 200mm",
     "image": "https://res.cloudinary.com/dx94piejq/image/upload/v1735200696/FLORA_PL_12_j0p2kt.jpg",
-    "brand": "Flora"
-  }
+    "brand": "Flora"
+  }
+
 ]
